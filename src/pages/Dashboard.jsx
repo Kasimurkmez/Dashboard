69219337cb269c29917aa7722b4db2a54cc4495a
kasimurkmez <kasimurkmez47@gmail.com>
2023-06-26 15:20:10 +0300

    updated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import "../styles/Dashboard.css";
+import SingleCard from "../components/reuseble/SingleCard";
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
+const carObj = {
+  title: "Total Cars",
+  icon: "ri-car-line",
+  totalNumber: 750,
+};
+const tripObj = {
+  title: "Daily Trips",
+  totalNumber: 1697,
+  icon: "ri-steering-2-line",
 };
 
+const clientObj = {
+  title: "Clients Annually",
+  totalNumber: "85k",
+  icon: "ri-user-line",
+};
+
+const distanceObj = {
+  title: "Kilometers Daily",
+  totalNumber: 2167,
+  icon: "ri-timer-flash-line",
+};
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <div className="dashboard__wrapper">
+        <div className="dashboard__cards">
+          <SingleCard item={carObj} />
+          <SingleCard item={tripObj} />
+          <SingleCard item={clientObj} />
+          <SingleCard item={distanceObj} />
+        </div>
+      </div>
+    </div>
+  );
+};
+ 
 export default Dashboard;
